0f301248006e06bb739eb875b332429a0df95c7d
Victor <onetworacks@gmail.com>
2024-12-01 12:28:11 +0000

    objects

---
+++ app.js
@@ -0,0 +1,10 @@
+let user = {
+    username: 'Victor',
+    email: 'itsvictorlopez@gmail.com',
+    subscriptionStatus: 'VIP',
+    discordId: 'Victor#000',
+}
+
+console.log(user.username[0]);
+
+console.log(user.subscriptionStatus)
